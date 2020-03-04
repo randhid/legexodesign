78bbc0dec2590be1b851a2bf1ca3c4d282e812f8
Complicated <cl3896@columbia.edu>
2020-03-04 01:08:31 -0500

    Gt

---
+++ pyfiles/Gt.py
@@ -0,0 +1,5 @@
+#Gt find the joint torque due to gravity
+#   TransM0 are the transformation matrices;
+#   BdWt is the weight of each body;
+#   BdCoM4 is the center of mass of each body in LCS;
+#   gv is the gravity vector, normally gv should be [0 0 -9.81].
