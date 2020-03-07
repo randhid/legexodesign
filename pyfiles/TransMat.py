df72a5af3b20b75a8e048ed1820e8fc759960507
Complicated <cl3896@columbia.edu>
2020-03-07 17:06:26 -0500

    TransMat ready

--- pyfiles/TransMat.py
+++ pyfiles/TransMat.py
@@ -1,16 +1,28 @@
 from math import *
+from DH import *
 import numpy
 
 #TRANSMAT Summary
 # transformation matrix
 
-## TODO: Understand the concept
+# Output a (nJoint+1, 4) size matrix
+
 
 def TransMat(nJoint, Dharray):
 
     TransM0 = numpy.zeros((4*(nJoint+1), 4))
 
-    
+    TransM0[0:4] = numpy.eye(4)
+
+    for i in range(nJoint):
+        TransM0[(i+1)*4:(i+1)*4+4] = numpy.dot( TransM0[i*4:(i+1)*4], DH(Dharray[i]) )
+
+    return TransM0
 
 
-    pass
+if __name__ == "__main__":
+
+    nJoint = 2
+    Dharray = numpy.array(((0, 1, 2, 3),(4, 5, 6, 7)))
+
+    print(TransMat(nJoint, Dharray))
