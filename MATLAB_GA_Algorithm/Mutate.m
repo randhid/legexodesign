be3d73a3d1c0d90982295b510b388ecdb9a73645
Chong Liu <55459522+complicat9716@users.noreply.github.com>
2020-03-13 11:44:42 -0400

    Delete Mutate.m

--- MATLAB_GA_Algorithm/Mutate.m
+++
@@ -1,8 +0,0 @@
-function offspring_mutated = Mutate(offspring, mutation_rate)
-
-    flag = (rand(size(offspring)) < mutation_rate);
-    
-    offspring_mutated = offspring;
-    offspring_mutated(flag) = 1 - offspring(flag);
-
-end
